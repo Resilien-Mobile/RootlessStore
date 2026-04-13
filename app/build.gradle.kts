81259e0700d3f86ab7624d4ae3ad810ebe9fb23e
Baidaidai_GFWD <1302064396@qq.com>
2026-04-13 13:30:06 +0800

    docs: Write change log about v0.0.7, Modify version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.5"
+        versionName = "0.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
