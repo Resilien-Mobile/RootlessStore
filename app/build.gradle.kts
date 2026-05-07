854e564c24d31941969cf68a5d77ece2f5703655
Baidaidai_GFWD <1302064396@qq.com>
2026-05-07 15:18:29 +0800

    chore(release): Bump version to v0.0.902

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 26
         targetSdk = 28
         versionCode = 1
-        versionName = "0.0.901"
+        versionName = "0.0.902"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
