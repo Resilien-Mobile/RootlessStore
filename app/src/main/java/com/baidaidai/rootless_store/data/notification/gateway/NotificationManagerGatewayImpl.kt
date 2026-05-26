86081fffa0119b15600fe690bd1cc3ead6afb8f5
Baidaidai_GFWD <1302064396@qq.com>
2026-05-26 20:35:48 +0800

    feat(notification): Add plugin status notification support
    Add notification permission, a status notification channel, and a default notification gateway.
    
    Use a shared notification channel id for plugin status notifications.

---
+++ app/src/main/java/com/baidaidai/rootless_store/data/notification/gateway/NotificationManagerGatewayImpl.kt
@@ -0,0 +1,58 @@
+package com.baidaidai.rootless_store.data.notification.gateway
+
+import android.Manifest
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.baidaidai.rootless_store.MainActivity
+import com.baidaidai.rootless_store.R
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class NotificationManagerGatewayImpl @Inject constructor(
+    @ApplicationContext
+    private val context: Context
+) {
+
+    fun pushDefaultNotification(
+        title: String,
+        message: String,
+        notificationId: Int = 1001
+    ){
+
+        val intent = Intent(context, MainActivity::class.java)
+
+        val pendingIntent = PendingIntent.getActivity(context,0,intent,PendingIntent.FLAG_IMMUTABLE)
+
+        val channel_id = context.getString(R.string.notification_channel_id)
+
+        if (
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU &&
+            ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        val notification = NotificationCompat.Builder(context, channel_id)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setAutoCancel(true)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        NotificationManagerCompat
+            .from(context)
+            .notify(notificationId, notification)
+
+    }
+
+}
