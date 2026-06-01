66d3063ed804b9d2dd85bc19029d49e506c54e58
Baidaidai_GFWD <1302064396@qq.com>
2026-06-01 19:25:12 +0800

    chore(release): Prepare v1.3.1 release
    Bump the app version from v1.3.0 to v1.3.1.
    
    Add release notes for remote plugin icon loading support.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 26
         targetSdk = 28
         versionCode = 1
-        versionName = "1.3.0"
+        versionName = "1.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
