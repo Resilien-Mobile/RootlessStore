cfe639e61d77bea2dccbd4c9832d69ee3f528011
Baidaidai_GFWD <1302064396@qq.com>
2026-06-04 23:07:45 +0800

    chore(release): Bump version to v1.4.0
    Update the app version display and add the v1.4.0 release notes.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 26
         targetSdk = 28
         versionCode = 1
-        versionName = "1.3.2"
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
