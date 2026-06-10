effad6a3a471b3fb9d850791eaa7b56b86f029ea
Baidaidai_GFWD <1302064396@qq.com>
2026-06-10 21:54:44 +0800

    feat(shell)!: Add shell command context memory
    Move shell command execution into the application layer.
    
    Add shell context configs for app shell and ADB shell.
    
    Remember the app shell current directory between commands.

---
+++ app/src/main/java/com/baidaidai/rootless_store/data/shell/provider/ShellExecuteContextProviderImpl.kt
@@ -0,0 +1,51 @@
+package com.baidaidai.rootless_store.data.shell.provider
+
+import com.baidaidai.rootless_store.data.environment.repository.EnvironmentRepositoryImpl
+import com.baidaidai.rootless_store.data.shell.repository.ShellPreferencesRepositoryImpl
+import com.baidaidai.rootless_store.domain.shell.model.AdbShellContextConfig
+import com.baidaidai.rootless_store.domain.shell.model.AppShellContextConfig
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class ShellExecuteContextProviderImpl @Inject constructor(
+    private val shellPreferencesRepositoryImpl: ShellPreferencesRepositoryImpl,
+    private val environmentRepositoryImpl: EnvironmentRepositoryImpl
+) {
+
+    suspend fun getAppShellContext(): AppShellContextConfig{
+        val preferences = shellPreferencesRepositoryImpl.shellContextPreferences.first()
+        val environmentPATH = environmentRepositoryImpl.getAvailableEnvironmentPath()
+        val environmentLDPATH = environmentRepositoryImpl.getAvailableEnvironmentLDPATH()
+        val environmentConfig = environmentRepositoryImpl.getAvailableEnvironmentConfig()
+
+        return AppShellContextConfig(
+            jumpToDirectory = preferences.jumpToDirectory,
+            environmentPATH = environmentPATH,
+            environmentLDPATH = environmentLDPATH,
+            environmentConfig = environmentConfig,
+        )
+    }
+
+    suspend fun getAdbShellContext(): AdbShellContextConfig{
+        val preferences = shellPreferencesRepositoryImpl.shellContextPreferences.first()
+        val environmentPATH = environmentRepositoryImpl.getAvailableEnvironmentPath()
+        val environmentLDPATH = environmentRepositoryImpl.getAvailableEnvironmentLDPATH()
+        val environmentConfig = environmentRepositoryImpl.getAvailableEnvironmentConfig()
+        val environmentConfigKeyList = environmentRepositoryImpl.getEnvironmentConfigKeyList()
+        val environmentConfigValueList = environmentRepositoryImpl.getEnvironmentConfigValueList()
+
+        return AdbShellContextConfig(
+            useRunAs = preferences.enableRunAs,
+            environmentPATH = environmentPATH,
+            environmentLDPATH = environmentLDPATH,
+            environmentConfig = environmentConfig,
+            environmentConfigKeyList = environmentConfigKeyList,
+            environmentConfigValueList = environmentConfigValueList,
+        )
+    }
+
+    fun getRootShellContext(){
+
+    }
+
+}
