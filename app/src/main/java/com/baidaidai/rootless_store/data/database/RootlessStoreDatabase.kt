efab664fbec0b7259a0b9ac4c1d8c81275cd2292
Creater. Bai <1302064396@qq.com>
2026-07-02 16:46:39 +0800

    chore(database): Remove legacy room migrations
    Remove old Room migration files.
    
    Reset the database version to the new major baseline.
    
    Stop registering legacy database migrations.

--- app/src/main/java/com/baidaidai/rootless_store/data/database/RootlessStoreDatabase.kt
+++ app/src/main/java/com/baidaidai/rootless_store/data/database/RootlessStoreDatabase.kt
@@ -27,7 +27,7 @@ import com.baidaidai.rootless_store.data.source.database.PluginSourceEntity
         CodeBrickEntity::class
         // 其它表也一起加进来
     ],
-    version = 5,
+    version = 1,
     exportSchema = true
 )
 @TypeConverters(RoomConvertRepositoryImpl::class)
@@ -41,29 +41,3 @@ abstract class RootlessStoreDatabase : RoomDatabase() {
 
     // 其它 DAO 也在这里暴露
 }
-
-
-//@Module
-//@InstallIn(SingletonComponent::class)
-//object DatabaseModule {
-//
-//    @Provides
-//    @Singleton
-//    fun provideDatabase(
-//        @ApplicationContext context: Context
-//    ): AppDatabase =
-//        Room.databaseBuilder(context, AppDatabase::class.java, "rootless_store.db")
-//            .build()
-//
-//    @Provides
-//    fun providePluginDao(db: AppDatabase): PluginInfoDAO = db.pluginDao()
-//}
-//
-//@Composable
-//fun e(){
-//    val k = Room.databaseBuilder(
-//        context = RootLessStoreLocalContext.current,
-//        klass = AppDatabase::class.java,
-//        name = "pluginInfoDataBase"
-//    ).build()
-//}
--- app/src/main/java/com/baidaidai/rootless_store/data/di/DataBaseHiltModule.kt
+++ app/src/main/java/com/baidaidai/rootless_store/data/di/DataBaseHiltModule.kt
@@ -3,10 +3,6 @@ package com.baidaidai.rootless_store.data.di
 import android.content.Context
 import androidx.room.Room
 import com.baidaidai.rootless_store.data.database.RootlessStoreDatabase
-import com.baidaidai.rootless_store.data.database.migration.MIGRATION_1_2
-import com.baidaidai.rootless_store.data.database.migration.MIGRATION_2_3
-import com.baidaidai.rootless_store.data.database.migration.MIGRATION_3_4
-import com.baidaidai.rootless_store.data.database.migration.MIGRATION_4_5
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,7 +24,7 @@ object DataBaseHiltModule {
             klass = RootlessStoreDatabase::class.java,
             name = "RootlessStoreDataBase"
         )
-            .addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4, MIGRATION_4_5)
+            .addMigrations()
             .build()
     }
 
