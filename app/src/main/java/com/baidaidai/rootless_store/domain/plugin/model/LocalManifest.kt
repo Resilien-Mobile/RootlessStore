6243091bb2ad3739e6215a8d0aca12d0b436fcf3
Creater. Bai <1302064396@qq.com>
2026-07-14 23:56:32 +0800

    feat(plugin): Add Magisk module installation support
    Recognize Magisk packages from their module.prop metadata.
    
    Convert supported modules and install their scripts as shell plugins.
    
    Save installed Magisk modules in the local plugin repository.

--- app/src/main/java/com/baidaidai/rootless_store/domain/plugin/model/LocalManifest.kt
+++ app/src/main/java/com/baidaidai/rootless_store/domain/plugin/model/LocalManifest.kt
@@ -1,5 +1,5 @@
 package com.baidaidai.rootless_store.domain.plugin.model
 
 enum class LocalManifest {
-    EnvironmentManifestLocal, PluginManifestLocal
+    EnvironmentManifestLocal, PluginManifestLocal, MagiskProp
 }
