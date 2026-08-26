31030325f7d4183954dac6017bfc787932b3c99b
Creater. Bai <1302064396@qq.com>
2026-08-26 20:49:23 +0800

    chore(release): Bump Rootless Store to version 2.2.2
    Update the package and displayed version for the 2.2.2 release.
    Add release notes for adaptive plugin workflows and copyable results.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         minSdk = 26
         targetSdk = 28
         versionCode = 2
-        versionName = "2.2.1"
+        versionName = "2.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
